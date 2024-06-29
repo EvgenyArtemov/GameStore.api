43e95096749601703fd731fb79cd470a42d3e8ff
Evgeny Artemov <evgeny.artemov@cteleport.com>
2024-06-29 17:05:23 +0400

    check for endpoint metadata

--- GameStore_api/Program.cs
+++ GameStore_api/Program.cs
@@ -1,6 +1,36 @@
+using GameStore_api.Dto;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.UseRouting();
+
+app.Use(async (context, next) =>
+{
+    var currentEndpoint = context.GetEndpoint();
+
+    if (currentEndpoint is null)
+    {
+        await next(context);
+        return;
+    }
+
+    Console.WriteLine($"Endpoint: {currentEndpoint.DisplayName}");
+
+    if (currentEndpoint is RouteEndpoint routeEndpoint)
+    {
+        Console.WriteLine($"  - Route Pattern: {routeEndpoint.RoutePattern}");
+    }
+
+    foreach (var endpointMetadata in currentEndpoint.Metadata)
+    {
+        Console.WriteLine($"  - Metadata: {endpointMetadata}");
+    }
+
+    await next(context);
+});
+
+    app.MapGet("/", () => "Route").WithDisplayName("Test Name");
 
 app.Run();
